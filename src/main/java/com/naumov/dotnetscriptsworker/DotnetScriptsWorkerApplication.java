3a1846fe208df3fcfd0d73fc3b5df3e41b8b16a2
semyonnaumov <semyon.naumov@gmail.com>
2023-01-24 17:08:59 +0400

    Bootstrap project

---
+++ src/main/java/com/naumov/dotnetscriptsworker/DotnetScriptsWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.naumov.dotnetscriptsworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotnetScriptsWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DotnetScriptsWorkerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naumov/dotnetscriptsworker/DotnetScriptsWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naumov.dotnetscriptsworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotnetScriptsWorkerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
