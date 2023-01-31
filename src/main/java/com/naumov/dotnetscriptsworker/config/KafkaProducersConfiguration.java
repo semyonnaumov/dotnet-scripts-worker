ddf023808810b3280e827db2a73871b779472e96
semyonnaumov <semyon.naumov@gmail.com>
2023-01-31 18:01:46 +0400

    Remove spring profile from KafkaProducersConfiguration

--- src/main/java/com/naumov/dotnetscriptsworker/config/KafkaProducersConfiguration.java
+++ src/main/java/com/naumov/dotnetscriptsworker/config/KafkaProducersConfiguration.java
@@ -8,7 +8,6 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -18,7 +17,6 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@Profile("!dev")
 @Configuration
 @EnableKafka
 public class KafkaProducersConfiguration {
