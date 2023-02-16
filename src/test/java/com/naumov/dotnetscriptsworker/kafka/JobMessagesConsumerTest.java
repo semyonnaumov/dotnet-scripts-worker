61e68b0f50a9f45a9ee96ba97ea27c28af1bb3ad
semyonnaumov <semyon.naumov@gmail.com>
2023-02-17 01:35:50 +0400

    Complete JobMessagesConsumerTest

--- src/test/java/com/naumov/dotnetscriptsworker/kafka/JobMessagesConsumerTest.java
+++ src/test/java/com/naumov/dotnetscriptsworker/kafka/JobMessagesConsumerTest.java
@@ -1,13 +1,17 @@
 package com.naumov.dotnetscriptsworker.kafka;
 
 import com.naumov.dotnetscriptsworker.dto.KafkaDtoMapper;
+import com.naumov.dotnetscriptsworker.dto.cons.JobConfig;
 import com.naumov.dotnetscriptsworker.dto.cons.JobTaskMessage;
+import com.naumov.dotnetscriptsworker.model.JobTask;
 import com.naumov.dotnetscriptsworker.service.JobService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.kafka.support.Acknowledgment;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -26,13 +30,30 @@ class JobMessagesConsumerTest {
 
     @Test
     void onJobTaskMessageRegular() {
-        JobTaskMessage jobTaskMessage = JobTaskMessage.builder().build();
+        UUID jobId = UUID.randomUUID();
+        String script = "script";
+        String nugetConfigXml = "<config />";
+        JobTaskMessage jobTaskMessage = JobTaskMessage.builder()
+                .jobId(jobId)
+                .script(script)
+                .jobConfig(JobConfig.builder().nugetConfigXml(nugetConfigXml).build())
+                .build();
+
         Acknowledgment ackMock = mock(Acknowledgment.class);
 
         jobMessagesConsumer.onJobTaskMessage(jobTaskMessage, ackMock);
 
+        ArgumentCaptor<JobTask> jobTaskCaptor = ArgumentCaptor.forClass(JobTask.class);
         verify(ackMock, times(1)).acknowledge();
-        verify(jobServiceMock, times(1)).runJob(any());
+        verify(jobServiceMock, times(1)).runJob(jobTaskCaptor.capture());
+        JobTask jobTask = jobTaskCaptor.getValue();
+        assertNotNull(jobTask);
+        assertNotNull(jobTask.getMessageId());
+        assertEquals(jobId ,jobTask.getJobId());
+        assertEquals(script ,jobTask.getJobScript());
+        JobTask.JobConfig jobConfig = jobTask.getJobConfig();
+        assertNotNull(jobConfig);
+        assertEquals(nugetConfigXml, jobConfig.getNugetConfigXml());
     }
 
     @Test
